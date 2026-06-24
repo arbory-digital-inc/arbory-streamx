5292003c71e33f5c3c943c06d2a8ccc7d0da4ec3
Kamil Chojak <kamil.chojak@ds.pl>
2026-06-24 10:04:33 +0200

    SXPR-297 Fix OpenSearch scripts

--- mesh/configs/opensearch/configuration/search-templates/format-search-templates.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source ../utils.sh
-
-format_search_template V1.0.0.6.5__eds_pages_search_template
---
+++ scripts/opensearch/configuration/search-templates/format-search-templates.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source ../utils.sh
+
+format_search_template V1.0.0.6.6__eds_pages_search_template
